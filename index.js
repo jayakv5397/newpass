d450c4b32533d57e4ee0f45db9345c8233efef4a
jayakv5397 <jayakv5397@gmail.com>
2023-01-30 11:49:04 +0530

    adding files from local repository

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-
-
-
 function random(selection,characters) {
     var pwd = [];
     password='';
@@ -86,9 +83,31 @@ function random(selection,characters) {
             break;
     }
 
-
-
 }
+
+$("#myElem").hide();
+
+// get the text from the DOM Element: 
+const textToCopy = $("#password").text();
+
+
+// when someone clicks on the <a class="copy-text"> element 
+// (which should be a <button>), execute the copy command:
+document.querySelector('.copy-text').addEventListener('click' , ()=> {
+    navigator.clipboard.writeText(textToCopy).then(
+      function() {
+        /* clipboard successfully set */
+
+        window.alert('Success! The text was copied to your clipboard').fadeOut(100);
+        $("#myElem").show("slow").delay(100).hide("slow");
+
+      }, 
+      function() {
+        /* clipboard write failed */
+        window.alert('Opps! Your browser does not support the Clipboard API')
+      }
+    )
+  })
 
 // random(10);
 
@@ -114,4 +133,5 @@ function random(selection,characters) {
         $("#test").val("0")
     }
   });
+
 
